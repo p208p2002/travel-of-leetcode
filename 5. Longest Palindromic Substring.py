075b315d4655626f6324f4a8eee0f3f3bb70c7d2
unknown <p208p2002@gmail.com>
2019-06-03 10:57:12 +0800

    no message

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -1,34 +1,50 @@
 class Solution:
     def judgePalindromic(self,s:str):
-        if(len(s)==2 and s[0] == s[1]):
+        if(len(s)<=1):
             return True
-        elif(len(s)==2 and s[0] != s[1]):
-            return False
-        elif(len(s)==0 or len(s)==1):
-            return True
+        loop = 0
+        sLen = len(s)
+        while(sLen>=2):
+            # print(loop,s)
+            head = s[0]
+            end = s[sLen-1]
 
-        head = s[0]
-        end = s[len(s)-1]
-        while(1):
-            if(head == end):
-                return self.judgePalindromic(s[1:-1])
+            if(s[0] == s[sLen-1]):
+                s = s[1:-1]
+                sLen = len(s)
+            elif(s[0] != s[sLen-1]):
+                return False
             else:
                 return False
-        return False
+            loop+=1
+        return True
+
 
     def longestPalindrome(self, s: str) -> str:
+        # print(self.judgePalindromic(s))
+
         if(s == ''):
             return ''
         headIndex = 0
         rec = s[0]
+        sLen = len(s)
+        rLen = len(rec)
+
         while(len(s) != (headIndex+1)):
-            ###
-            for i in range(len(s),0,-1):
-                if(s[headIndex] == s[i-1]):
+            for i in range(sLen,0,-1):
+                if(headIndex == i-1):
+                    break
+                elif(rLen>=sLen-(headIndex)):
+                    return rec
+                elif(s[headIndex] == s[i-1]):
                     testStr = s[headIndex:i]
-                    #print(testStr,rec)
-                    if(self.judgePalindromic(testStr) and (len(testStr) > len(rec))):
-                            rec = testStr
+                    tLen = len(testStr)
+                    if(tLen<rLen):
+                        continue
+                    elif(self.judgePalindromic(testStr) and (tLen > rLen)):
+                        # print(testStr,rec)
+                        rec = testStr
+                        rLen = len(rec)
                     #break
             headIndex += 1
         return rec
